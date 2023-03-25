a1b2087300565af2d311d9370eec897bb5de5fd5
shichiye <shichiye@126.com>
2023-03-25 19:28:59 +0800

    feat: mongo

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Inject, Injectable } from '@nestjs/common';
+import { MongoRepository } from 'typeorm';
+import { User } from './entities/user.mongo.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @Inject('USER_REPOSITORY')
+    private userRepository: MongoRepository<User>,
+  ) { }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  createOrSave(user: User) {
+    return this.userRepository.save(user)
   }
 }
